7e525831dc5c31b6fac640806fb94f638ff3d643
zenklot <zenklot11@gmail.com>
2022-12-31 16:02:17 +0700

    advance constructor

--- src/constructor.ts
+++ src/constructor.ts
@@ -1,7 +1,22 @@
 class Animals {
-    constructor() {
-        console.log('Constructor dipanggil dahulu')
+    name : string;
+    leg: number;
+    constructor(name: string, leg: number) {
+        this.name = name;
+        this.leg = leg;
     }
 }
 
-const anjing = new Animals()
+class Home {
+    constructor(public name: string, public color: string) {}
+}
+
+const anjing = new Animals('heli', 4)
+console.log(anjing)
+const ayam = new Animals('rembo', 2)
+console.log(ayam)
+
+const minimalis = new Home('minimalis', 'red')
+console.log(minimalis)
+const Mewah = new Home('Mewah', 'green')
+console.log(Mewah)
