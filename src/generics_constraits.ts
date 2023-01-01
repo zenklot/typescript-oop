d9f7be1a1280165c6fb0c2a85160a8a916d25cbd
zenklot <zenklot11@gmail.com>
2023-01-01 07:28:58 +0700

    generics class

--- src/generics_constraits.ts
+++ src/generics_constraits.ts
@@ -18,3 +18,29 @@ console.log(generics3a)
 
 const generics3b = generics3({length: 10, value: 122});
 console.log(generics3b)
+
+
+
+interface Produk {
+    sell() : void
+}
+
+class CarProduct implements Produk {
+    sell() : void {
+        console.log('sell mobil')
+    }
+}
+
+class MotorProduct implements Produk {
+    sell() : void {
+        console.log('sell motor')
+    }
+}
+
+function genericsConstrait<T extends Produk> (products: T[]) {
+    products.forEach(product => {
+        product.sell()
+    })
+}
+
+genericsConstrait([new MotorProduct(), new MotorProduct(), new CarProduct()])
