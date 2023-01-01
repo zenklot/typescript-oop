de140ce8205271276d6f75431e510784efb4699a
zenklot <zenklot11@gmail.com>
2023-01-01 07:17:17 +0700

    generics in class

--- src/generics_class.ts
+++ src/generics_class.ts
@@ -14,3 +14,18 @@ class GenericsClass<T> implements Generics3<T> {
         return this.propA;
     }
 }
+
+
+class ClassA {
+    name : string = "ClassA";
+}
+
+class ClassB<T> {
+    classProps: T
+    constructor(classProps: T) {
+        this.classProps = classProps;
+    }
+}
+
+const classA = new ClassA()
+const classB = new ClassB(classA);
