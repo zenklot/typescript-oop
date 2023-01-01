96891d23b9240bda09ba9c3cb8163839f1894f2c
zenklot <zenklot11@gmail.com>
2023-01-01 07:32:53 +0700

    parameter keyof di generics

--- src/generics_constraits.ts
+++ src/generics_constraits.ts
@@ -44,3 +44,16 @@ function genericsConstrait<T extends Produk> (products: T[]) {
 }
 
 genericsConstrait([new MotorProduct(), new MotorProduct(), new CarProduct()])
+
+
+console.log('===============================================================')
+
+function getProperty<T, U extends keyof T>(obj: T, key: U) {
+    return obj[key]
+}
+
+let x = { a: 1, b: 2, c: 3, d: 4}
+
+console.log(getProperty(x, 'a'))
+console.log(getProperty(x, 'b'))
+console.log(getProperty(x, 'd'))
